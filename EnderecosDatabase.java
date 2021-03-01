500ca37abf7cceee0174670933f4375099018a09
marcioFreire42 <51244714+marcioFreire42@users.noreply.github.com>
2021-03-01 20:56:43 -0300

    Update EnderecosDatabase.java
    Adicionado comentários explicando os parâmetros do @Database

--- EnderecosDatabase.java
+++ EnderecosDatabase.java
@@ -7,6 +7,9 @@ import com.example.delivers.arquivosDAO.RoomDao;
 import com.example.delivers.objetos.Endereco;
 
 @Database(entities = {Endereco.class}, version = 1, exportSchema = false)
+    // entities representa o objeto que servirá de base para este Database;
+    // Version é o número da versão, podemos usar o 1 mesmo;
+    // exportSchema false é para não ficar criando automaticamente arquivos jason;
 public abstract class EnderecosDatabase extends RoomDatabase {
 
     public abstract RoomDao getRoomEnderecoDao();
