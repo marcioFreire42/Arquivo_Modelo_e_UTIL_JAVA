a411e68452542e7baef1f5abd49221cbd6c0b6e5
marcioFreire42 <51244714+marcioFreire42@users.noreply.github.com>
2021-03-09 22:53:16 -0300

    Adicionado getInstance()
    Método getInstance( ) cria o dataBase e pode criar o roomDao;

--- EnderecosDatabase.java
+++ EnderecosDatabase.java
@@ -13,5 +13,15 @@ import com.example.delivers.objetos.Endereco;
 public abstract class EnderecosDatabase extends RoomDatabase {
 
     public abstract RoomDao getRoomEnderecoDao();
+    
+    public static EnderecosDatabase getIntance(Context contexto){
+        return Room.databaseBuilder(context, JogadoresDatabase.class, "Enderecos.db")
+        .allowMainThreadQueries().build();
+    }
+        // Este método tem que ser refeito em toda classe que faz uso do database, o que esse método faz é receber essa missão;
+        // Agora na classe basta setar o database com este método  
+        // database = EnderecoDatabase.getInstance(this);
+        // E esse método tambéms serve para criar o roomDao, com o seguinte seta:
+        //  roomDatabase = EnderecoDatabase.getInstance(this).getRoomEnderecoDao();
 
 }
